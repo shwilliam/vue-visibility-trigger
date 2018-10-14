53d09602653ec782b2afeabd3ade4e42e6dff841
William L <w-lindvall@outlook.com>
2018-10-14 19:22:25 +1100

    oss vue sfc boiler

---
+++ examples/example-1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <VueScrollState />
+  </div>
+</template>
+
+<script>
+import VueScrollState from "../src/vue-scroll-state.vue";
+export default {
+  name: "Example1",
+  data() {
+    return {};
+  },
+  components: {
+    VueScrollState
+  }
+};
+</script>
---
+++ src/vue-scroll-state.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "VueScrollState",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<template>
+    <div class="vue-scroll-state">
+        hello world
+    </div>
+</template>
+
+<style scoped>
+</style>
